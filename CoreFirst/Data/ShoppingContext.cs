b72af58b908a216a7a7107e1da4048efbc98ba6b
SamirSalmanov <samir_salmanov_1988@mail.ru>
2021-06-24 03:27:05 -0700

    Add project files.

---
+++ CoreFirst/Data/ShoppingContext.cs
@@ -0,0 +1,24 @@
+﻿using CoreFirst.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreFirst.Data
+{
+    public class ShoppingContext:DbContext
+    {
+        public ShoppingContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Picture> Pictures { get; set; }
+        public DbSet<ProductPicture> ProductPictures { get; set; }
+
+
+
+    }
+}
---
+++ CoreFirst/Models/ProductPicture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreFirst.Models
+{
+    public class ProductPicture
+    {
+        public int ID { get; set; }
+        public int PictureID { get; set; }
+        public virtual Picture Picture { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        
+
+
+    }
+}
